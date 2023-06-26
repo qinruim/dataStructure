6c43bab2b74d0fa4adabe897e3a863ef9971c729
qrpop <mqinrui@163.com>
2023-06-26 21:45:06 +0800

    tp

---
+++ 00-leetCode/src/leetcode/editor/cn/P424_LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,25 @@
+package leetcode.editor.cn;
+
+/**
+ * 替换后的最长重复字符
+ * @author mqinrui
+ * @date 2023-06-26 20:04:45
+ */
+public class P424_LongestRepeatingCharacterReplacement{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P424_LongestRepeatingCharacterReplacement().new Solution();
+	 }
+	 
+//力扣代码提交区
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+
+		return  0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
