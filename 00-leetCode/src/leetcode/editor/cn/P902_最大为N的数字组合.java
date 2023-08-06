7b9ce4a77856c2bfeb4592e042a7536841bac5a3
qrpop <mqinrui@163.com>
2023-08-06 21:08:54 +0800

    小红书笔试

---
+++ 00-leetCode/src/leetcode/editor/cn/P902_最大为N的数字组合.java
@@ -0,0 +1,25 @@
+package leetcode.editor.cn;
+
+/**
+ * 最大为 N 的数字组合
+ * numbers-at-most-n-given-digit-set
+ * @author mqinrui
+ * @date 2023-08-05 16:14:47
+ */
+class P902_NumbersAtMostNGivenDigitSet{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P902_NumbersAtMostNGivenDigitSet().new Solution();
+	 }
+
+//力扣代码提交区
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
