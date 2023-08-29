b452813164ccced0685a1e8949b1fb3ddc8e82b2
qrpop <mqinrui@163.com>
2023-08-29 19:14:17 +0800

    8.29每日一题:823

---
+++ 00-leetCode/src/leetcode/editor/cn/P823_带因子的二叉树.java
@@ -0,0 +1,28 @@
+package leetcode.editor.cn;
+
+/**
+ * 带因子的二叉树
+ * binary-trees-with-factors
+ * @author mqinrui
+ * @date 2023-08-29 17:22:24
+ */
+class P823_BinaryTreesWithFactors{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P823_BinaryTreesWithFactors().new Solution();
+	 }
+
+//力扣代码提交区
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numFactoredBinaryTrees(int[] arr) {
+
+
+
+		return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
